b20a270cdf9be7510197175ad0b80ee1aa0cb4b3
Gabriel dos Passos <gabrielp@MacBook-Air-de-Gabriel.local>
2023-04-05 13:35:06 -0300

    Create get user profile service and changed InvalidCredentialsError file name

--- readme.md
+++ readme.md
@@ -5,7 +5,7 @@ App estilo GymPass
 ## RFs (Requisitos funcionais)
 
 - [x] Deve ser possível se cadastrar;
-- [ ] Deve ser possível se autenticar;
+- [x] Deve ser possível se autenticar;
 - [ ] Deve ser possível obter um perfil de um usuário logado;
 - [ ] Deve ser possível obter o número de check-ins realizados pelo usuário;
 - [ ] Deve ser possível o usuário obter seu histórico de check-ins;
@@ -27,6 +27,6 @@ App estilo GymPass
 ## RNFs (Requisitos não-funcionais)
 
 - [x] A senha do usuário precisa estar criptografada;
-- [ ] Os dados da aplicação precisam estar persistidos em um banco POSTGRESQL;
+- [x] Os dados da aplicação precisam estar persistidos em um banco POSTGRESQL;
 - [ ] Todas as listas de dados precisam estar paginadas com 20 itens por página;
 - [ ] O usuário deve ser identificado por um JWT;
---
+++ src/services/get-user-profile.spec.ts
@@ -0,0 +1,38 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { hash } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { GetUserProfileService } from './get-user-profile'
+import { ResourceNotFoundError } from './errors/resource-not-found'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileService
+
+describe('Get User Profile Service', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileService(usersRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'Jhon Doe',
+      email: 'jhondoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+    expect(user.name).toEqual('Jhon Doe')
+  })
+
+  it('should not be able to get user profile with wrong id', async () => {
+    expect(() =>
+      sut.execute({
+        userId: 'invalid-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
