6b2d4f6ae1a49213c0668fdf29210cc0fc7e9458
Gabriel dos Passos <gabrielp@MacBook-Air-de-Gabriel.local>
2023-04-07 15:48:56 -0300

    Create search gyms and search nearby gyms tests, and changed the parse schema to request.query

---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,56 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Search Gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to search gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description',
+        phone: '199999999',
+        latitude: -27.5944702,
+        longitude: -48.5893463,
+      })
+      .set('Authorization', `Bearer ${token}`)
+
+    await request(app.server)
+      .post('/gyms')
+      .send({
+        title: 'TypeScript Gym',
+        description: 'Some description',
+        phone: '199999999',
+        latitude: -27.5944702,
+        longitude: -48.5893463,
+      })
+      .set('Authorization', `Bearer ${token}`)
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        query: 'JavaScript',
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'JavaScript Gym',
+      }),
+    ])
+  })
+})
