03488d82022d82a3ef4ebdba6d1baef85f710cdd
Gabriel dos Passos <gabrielp@MacBook-Air-de-Gabriel.local>
2023-04-07 16:09:53 -0300

    Create check-ins controller tests and fix metrics route name

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -9,7 +9,7 @@ export async function checkInsRoute(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.get('/check-ins/history', history)
-  app.get('/check-ins/metrcis', metrics)
+  app.get('/check-ins/metrics', metrics)
 
   app.post('/gyms/:gymId/check-ins', create)
   app.patch('/check-ins/:checkInId/validate', validate)
